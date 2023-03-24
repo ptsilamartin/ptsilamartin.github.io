caa7a76f35a16072194d8c78301bbec7542fe9f1
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-03-24 07:24:01 +0100

    TP 12

---
+++ info/TP/TP12.py
@@ -0,0 +1,89 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+### question 1
+
+
+### question 2
+
+
+
+### question 3
+
+
+### question 4
+def plot_graphe_l(G):
+    Gx=nx.Graph()
+    edges=aretes_l(G)
+    Gx.add_edges_from(edges)
+    nx.draw(Gx,with_labels = True)
+    plt.show()
+
+
+# plt.figure('graphe non pondéré')
+# plot_graphe_l(G)
+
+
+### question 5
+
+
+
+### question 6
+
+
+
+### question 7
+
+
+
+### exercice 2
+### question 8
+
+
+
+### question 9
+
+
+
+### question 10
+
+
+### pour aller plus loin, mettre le poids de chaque arête
+
+### question 11
+
+
+
+### pour aller plus loin, mettre le poids de chaque arête
+
+
+
+
+
+### question 12
+
+
+
+### question 13
+
+
+### question 14
+
+
+### question 15
+
+
+### question 16
+
+
+### question 17
+
+
+### question 18
+
+
+
+
+
+
+
