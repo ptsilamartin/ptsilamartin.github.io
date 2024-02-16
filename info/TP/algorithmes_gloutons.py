f6d31fc89fb43e06b0c5f8bf8418afcf1597d803
Xavier Pessoles <xpessoles.ptsi@free.fr>
2024-02-16 15:48:58 +0100

    MAJ

---
+++ info/TP/algorithmes_gloutons.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 16:29:39 2021
+
+@author: nicov
+"""
+
+
+
+# fruit de type [prix au kilo en euros : int, nom : str, quantité en kg : int]
+cueillette = [ [24,"framboises",1], [16,"myrtilles",3], [6,"fraises",5], [3,"mures",2] ]
+
+
+
+### question 1
+def sac_a_dos(L,capacite):
+    """Implémentation de la résolution du problème du sac à dos
+    L est une liste des éléments à mettre dans le sac
+    capacité est la masse maximale qu peut transporter le sac à dos.
+    """
+    masse_sac = 0
+    sac=[]                                        #à l'initialisation, le sac est vide
+    i=0                                           #i sert d'indice dans la liste L
+                                     
+    while .................. and ................:#tant qu'on n'a pas parcouru toute la cueillette 
+                                                  #et que la masse du sac n'a pas atteint sa capacité                              
+        fruit = L[i]   
+        capacite_restante = capacite - masse_sac                           
+        if ......................................:#si la quantité du i-eme fruit est supérieure à 
+                                                  #la capacité restante du sac
+
+            
+                                                  #que la quantité correspondant à la capacité restante
+                                                  
+        sac. ..............(fruit)                #On met le fruit dans le sac
+        
+        masse_sac = masse_sac + ..............    #On calcule la nouvelle masse du sac
+        
+        i=i+1       #On incrémente i pour passer au fruit suivant de la cueillette
+    return ......
+            
+        
+### question 2
+     
+        
+   
+### question 3  
+# fruit de type [prix au kilo en euros : int, nom : str, masse en kg : int, quantité : int]
+fruits_disponibles = [[3,"melon de cavaillon",1,1], [2.5,"melon jaune",2,1], [2,"pastèque",3,1]]
+
+
+
+### questions 4 et 5
+
+
+
+
+
+        
+
+### exercice 2 PCA
+# 8h45 est représenté par un flottant 8.45
+L=[[8,9,"C1"],[8.45,10.3,"C2"],[8.1,11.3,"C3"],[11.15,11.45,"C4"],[12,12.45,"C5"],[11,13,"C6"], [12.3,14,"C7"],[16,17,"C8"],[15,18,"C9"],[16.2,18.15,"C10"]]      
+        
+        
+        
+        
