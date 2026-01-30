38117b3508d2c51ebd3fa9526fb2cb2673b8ff13
Xavier Pessoles <xpessoles.ptsi@free.fr>
2026-01-30 16:06:02 +0100

    MAJ

---
+++ TP/TP_08.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import matplotlib . image as img # import de la bibliotheque image
+import numpy as np
+
+##Question 1 : création d'une image noire
+def image_noire(n,p):
+    '''construit la matrice n*p d'une image noire'''
+    img =[0]*n    #construit une liste contenant A COMPLETER
+    for i in range(n): # n sera le nombre de colonne/ de lignes A COMPLETER
+        img[i]=[0]*p #construit une liste contenant A COMPLETER et l'affecte à A COMPLETER
+    return img   #img sera une image noire de hauteur : ??? pixels et de largeur ??? pixels  (A COMPLETER EN INDIQUANT LE NB DE PIXELS)
+
+
+
+##Questio2 : affichage
+
+
+
+
+
+##Question : modifier une image pour l'éclaircir
+def eclaircir(im,k):
+
+
+    return(im)
+
+
+
+##Question : modifier une image pour l'éclaircir en faisant une copie indépendante
+def eclaircir2(im0,k):
+
+    return(im)
+
+
+
+##Question : lecture d'un fichier image et appel de la fonction éclaircir
+
+
+
+##Question : lecture et affichage de l'image ciel
+
+
+
+##Question : détection de contour d'une image
+
+def contour (im, seuil):
+
+    return im2
+
+##Question : test
+
+
+
+#plt.show()
